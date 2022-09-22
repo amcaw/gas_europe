a29cc551567ca0bbd1b2b5a1025fd33292926286
amcaw <42608053+amcaw@users.noreply.github.com>
2022-09-22 16:11:09 +0200

    Update agsi.py

--- agsi.py
+++ agsi.py
@@ -11,20 +11,42 @@ headers = {
 
 response = requests.get('https://agsi.gie.eu/api?', headers=headers)
 
-myjson = response.json()
+ourdata_EU = []
 
-ourdata = []
+ourdata_NON_EU = []
 
-csvheader = ["name", "gasDayStart", "full"]
+csvheader = ["name", "gasDayStart", "full", "trend"]
+
+#EU countries
 
 for x in myjson ['data'][0]['children']:
-    listing = [x['name'],x['gasDayStart'],x['full']]
-    ourdata.append(listing)
+    listing = [x['name'],x['gasDayStart'],x['full'],x['trend']]
+    ourdata_EU.append(listing)
     
-df = pd.DataFrame (ourdata, columns = ['name', 'date', 'percentage_full'])
+#NON EU countries
 
-df_base = pd.read_csv('https://raw.githubusercontent.com/amcaw/gas_tanks/main/europe.csv')
+for x in myjson ['data'][1]['children']:
+    listing = [x['name'],x['gasDayStart'],x['full'],x['trend']]
+    ourdata_NON_EU.append(listing)
+    
+df_EU = pd.DataFrame (ourdata_EU, columns = ['country', 'date', 'percentage_full', 'trend'])
 
-df_all = pd.merge(df_base, df)
+df_NON_EU = pd.DataFrame (ourdata_NON_EU, columns = ['country', 'date', 'percentage_full', 'trend'])
 
-df_all.to_csv("./gas_data.csv")
+# Concat files
+
+df_ALL = pd.concat([df_EU, df_NON_EU])
+
+# Drop rows with empty values
+
+df_ALL = df_ALL[df_ALL.percentage_full != '-']
+
+# Merge base FR
+
+df_base = pd.read_csv('https://raw.githubusercontent.com/amcaw/gas_europe/main/base_fr.csv')
+
+df_ALL_FR = pd.merge(df_base, df_ALL)
+
+df_ALL_FR = df_ALL_FR[["name_fr", "percentage_full", "trend"]]
+
+df.to_csv("./gas.csv", index=False)
