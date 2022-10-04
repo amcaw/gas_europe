081f42f2ffdad246e46011426fe08ce86ed467d5
amcaw <42608053+amcaw@users.noreply.github.com>
2022-10-04 10:27:59 +0200

    Update agsi.py

--- agsi.py
+++ agsi.py
@@ -68,7 +68,7 @@ df_ALL_FR = pd.merge(df_base, df_ALL)
 
 # Select columns
 
-df_ALL_FR = df_ALL_FR[["name_fr", "in_storage", "date", "percentage_full", "trend", "latitude", "longitude"]]
+df_ALL_FR = df_ALL_FR[["name_fr", "in_storage", "date", "percentage_full", "trend", "latitude", "longitude", "total_TWh", "pourcentage_conso"]]
 
 # Cleaning to add up and down arrows
 
